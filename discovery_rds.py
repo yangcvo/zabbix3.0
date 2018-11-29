45ae7b7f2fd6dbbeae4d5b58c1d734240b7d8aef
Yancy <yancyanga@outlook.com>
2018-11-29 18:25:33 +0800

    Zabbix_AliyunRDS
    Zabbix_AliyunRDS

---
+++ discovery_rds.py
@@ -0,0 +1,35 @@
+#coding=UTF-8
+#Auther：Yancy.cc
+from aliyunsdkcore import client
+from aliyunsdkrds.request.v20140815 import DescribeDBInstancesRequest
+import json
+
+ID = 'ID'
+Secret = 'Secret'
+RegionId = 'cn-shenzhen'
+
+clt = client.AcsClient(ID,Secret,RegionId)
+
+DBInstanceIdList = []
+DBInstanceIdDict = {}
+ZabbixDataDict = {}
+def GetRdsList():
+    RdsRequest = DescribeDBInstancesRequest.DescribeDBInstancesRequest()
+    RdsRequest.set_accept_format('json')
+    #RdsInfo = clt.do_action(RdsRequest)
+    RdsInfo = clt.do_action_with_exception(RdsRequest)
+    for RdsInfoJson in (json.loads(RdsInfo))['Items']['DBInstance']:
+        DBInstanceIdDict = {}
+        try:
+            DBInstanceIdDict["{#DBINSTANCEID}"] = RdsInfoJson['DBInstanceId']
+            DBInstanceIdDict["{#DBINSTANCEDESCRIPTION}"] = RdsInfoJson['DBInstanceDescription']
+            DBInstanceIdList.append(DBInstanceIdDict)
+        except Exception, e:
+            print Exception, ":", e
+            print "Please check the RDS alias !Alias must not be the same as DBInstanceId！！！"
+            
+
+
+GetRdsList()
+ZabbixDataDict['data'] = DBInstanceIdList
+print json.dumps(ZabbixDataDict)
